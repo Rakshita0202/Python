801dd36e432524d109322a7a86c17f6c5f1ffc42
Rakshita <rathirakshita.02@gmail.com>
2026-06-23 18:16:07 +0530

    Done my work

---
+++ Operators.py
@@ -0,0 +1,84 @@
+#Arithmetic Operator
+#+,-,*,/,//,%,**
+
+a = 10
+b = 20
+
+#Addition
+c = a+b
+print(c)
+
+#Subtraction
+d = a-b
+print(d)
+
+#Multiplication
+e = a*b
+print(e)
+
+#Division
+f = a/b
+print(f)
+
+#Floor division
+x=15
+y=10
+print(x/y)
+print(x//y)
+
+#Modulo operator
+#Remainder
+print(x%y)
+
+#Exponential
+#2^3
+a=2
+b=3
+print(a ** b)
+
+#Comparison Operator
+#<,<=,>,>=,==,!=
+#Comparison operators gives the boolean value means true or false
+
+#Less than '<'
+a = 20
+b = 30
+print(a<b)
+
+#More than '>'
+a = 20
+b = 10
+print(a>b)
+
+#Less than or equal '<='
+a = 80
+b = 10
+print(a<=b)
+
+#More than or equal '>='
+a = 20
+b = 20
+print(a>=b)
+
+#equal '=='
+a = 20
+b = 30
+print(a==b)
+
+#not equal '!='
+a = 1
+b = 1
+print(a!=b)
+
+#Logical operator
+#and , or , not
+
+#and, or
+a = 11
+print((10 < a) and (a <20))
+print((10< a) or (a>20))
+
+#not
+a = 20
+print (not(25 > a))
+
